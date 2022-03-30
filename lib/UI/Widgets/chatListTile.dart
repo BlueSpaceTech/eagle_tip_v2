777f1760151a1cf79abc02bc005be1c90d3520b9
Abhishek Saini <78199221+Abhisheksainii@users.noreply.github.com>
2022-03-30 10:52:01 +0530

    fix

--- lib/UI/Widgets/chatListTile.dart
+++ lib/UI/Widgets/chatListTile.dart
@@ -134,8 +134,7 @@ class _ChatListTileState extends State<ChatListTile> {
             ),
           ),
           Visibility(
-            visible: (widget.doc.get("isNew").toString().isEmpty ||
-                widget.doc.get("isNew") != user.uid),
+            visible: widget.doc.get("isNew") != user.uid,
             child: Container(
               alignment: Alignment.center,
               width: 20,
